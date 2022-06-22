eaf23cb8755cdd778cdaf2bcec56bd8f1adcad16
Valentyn Boginskey <valentyn@greenhouse.io>
2022-06-22 13:38:51 -0600

    Lower minimum search interval

--- camply/config/search_config.py
+++ camply/config/search_config.py
@@ -12,8 +12,8 @@ class SearchConfig:
     File Path Storage Class
     """
 
-    POLLING_INTERVAL_MINIMUM: int = 5  # 5 MINUTES
-    RECOMMENDED_POLLING_INTERVAL: int = 10  # 10 MINUTES
+    POLLING_INTERVAL_MINIMUM: int = 1  # MINUTES
+    RECOMMENDED_POLLING_INTERVAL: int = 10  # MINUTES
     ERROR_MESSAGE: str = "No search days configured. Exiting"
     MINIMUM_CAMPSITES_FIRST_NOTIFY: int = 5
     MAXIMUM_NOTIFICATION_BATCH_SIZE: int = 20
